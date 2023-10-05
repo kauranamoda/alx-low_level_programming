546aba24c8c6a33bc69be947da9b4d8164c3b883
Babangida <kauranamoda@yahoo.com>
2023-10-05 16:54:57 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d bytes(s)\n", sizeof(b));
-printf("Size of a long int: %d bytes(s)\n", sizeof(c));
-printf("Size of a long long int: %d bytes(s)\n", sizeof(d));
-printf("Size of a float: %d bytes(s)\n", sizeof(e));
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", sizeof(e));
 return (0);
 }
