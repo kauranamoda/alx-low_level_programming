afb5d88d88b327b059fed88ef1ec059decf4c815
Babangida <kauranamoda@yahoo.com>
2023-10-06 16:22:49 -0700

    tolerate

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,12 +19,12 @@ int main(void)
 	{
 		printf("The last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	if (last_digit < 6 && last_digit != 0)
 	{
 		printf("The last digit of %d is %d and is less than 6 and not 0\n",
 			   n, last_digit);
 	}
-	else
+	if (last_digit == 0)
 	{
 		printf("The last digit of %d is %d and is 0\n", n, last_digit);
 	}
