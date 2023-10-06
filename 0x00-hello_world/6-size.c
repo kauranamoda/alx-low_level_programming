fe2a07c4319acca5b81f6f5645c48031cdaedbf5
Babangida <kauranamoda@yahoo.com>
2023-10-05 17:09:58 -0700

    jahwa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-printf("Size of a long int: %lu bytes(s)\n", sizeof(long));
-printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long));
-printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long));
+printf("Size of a float: %d bytes(s)\n", sizeof(float));
 return (0);
 }
