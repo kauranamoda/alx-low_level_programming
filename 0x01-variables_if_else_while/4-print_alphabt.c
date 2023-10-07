c7f520ced381fbed434b6e5f251a4bdeeb651978
Babangida <kauranamoda@yahoo.com>
2023-10-07 16:05:08 -0700

    manya

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The brain of the project
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
