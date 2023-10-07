69bcc5d289696de6604cdb8f649e62adf392a812
Babangida <kauranamoda@yahoo.com>
2023-10-06 17:12:59 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-somethig
+#include <stdio.h>
+/**
+ * main - Doing all the work
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char lettrs[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (int i = 0; i < 52; i++)
+
+	{
+		putchar(lettrs[i]);
+	}
+	putchar('\n');
+	return (0);
+}
