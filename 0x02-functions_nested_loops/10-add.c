ae184509b657ea1d551406d72456e6389a1edcb5
Babangida <kauranamoda@yahoo.com>
2023-10-10 19:29:48 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - the brain powers the functionality
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
