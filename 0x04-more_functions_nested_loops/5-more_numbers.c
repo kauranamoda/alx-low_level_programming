2efd126eafd1967f75ceee18eb486507f6cdf9d7
Babangida <kauranamoda@yahoo.com>
2023-10-12 17:17:37 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
- * more_numbers - func prints 10 times the numbers, from 0 t0 14
- * followed by a new line
+ * more_numbers - the brain func
+ * Return: no return
  */
+
 void more_numbers(void)
 {
-	int i, j;
 
-	for (i = 0; i < 10; i++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
