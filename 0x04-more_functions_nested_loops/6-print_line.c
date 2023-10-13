dab06193c3030a22f97e33169e530cd21abdba89
Babangida <kauranamoda@yahoo.com>
2023-10-12 17:19:57 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
-* print_line - func draws a stright line in the terminal
-* @n: number of times the character _ should be printed
-*/
+ * print_line - the brain behind printing lines
+ * @n: number of times the _ is printed
+ *
+ * Return: void
+ */
+
+
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+	int i;
 
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-			_putchar('\n');
-		}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
-}
+	_putchar('\n');
+}i
