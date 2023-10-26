5dd14e3ad51a8f148baa180ea22c0c85bc64bc19
Babangida <kauranamoda@yahoo.com>
2023-10-26 03:44:25 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,14 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: string
- * Return 0;
+ * Return: 0;
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s)
+	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
+	}
 }
